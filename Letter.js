643968afe1cf254fb112aed7195ca7faf7155369
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-29 16:14:56 -0400

    work in progress

--- Letter.js
+++ Letter.js
@@ -1,29 +1,36 @@
+// create a class called letter
 class Letter {
-  constructor (letterValue){
-    stringValue =letterValue
-    guess = false
+  constructor(letterValue) {
 
-    guessLetter (guessedLetter); {
-      if (this.stringValue === guessedLetter){
-        this.guessed = true;
-      }
-    }
-
-    renderLetter (); {
-      if (guessed === false){
-        console.log("_")
-        return "_";
-      }
-      else {
-        console.log(letter)
-        return letter;
-      }
-      
-    }
-  
+    // this. must be used, it accesses a variable for THIS instance
+    this.stringValue = letterValue
+    this.guess = false
+    // the constructor should close here, it does not define functions.
   }
+
+  guessLetter(guessedLetter) {
+    if (this.stringValue === guessedLetter) {
+      this.guess = true;
+    }
+  }
+
+  // replaced if else statement with ternary 
+  renderLetter() {
+    return this.guess ? this.stringValue : "_"
+  }
+
 }
 
 module.exports = {
   Letter
 }
+
+
+
+// A string value to store the underlying character for the letter
+
+// A boolean value that stores whether that letter has been guessed yet
+
+// A function that returns the underlying character if the letter has been guessed, or a placeholder(like an underscore) if the letter has not been guessed
+
+// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
